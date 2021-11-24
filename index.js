ca8eb4e29828f6ea97a11dc0821cc55fb4a950ff
SamJones1995 <88803371+SamJones1995@users.noreply.github.com>
2021-11-24 16:52:43 -0500

    Express static middleware for documentation get

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 const express = require('express');
 const app = express();
+
+let topHorror = [
+  {
+    title:  'The Ritual',
+    director: 'David Bruckner'
+  }
+]
+
+app.get('/',(req,res) => {
+  res.send('Welcome to myHorror!');
+});
+
+app.get('/movies',(req,res) => {
+  res.json(topHorror);
+});
+
+//middleware
+app.use(express.static('public'));
+
+//get documentation 
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.listen(8080, () =>{
+  console.log('This app is listening on port 8080.');
+});
