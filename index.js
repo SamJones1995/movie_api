3fb76104d3f062b6fea0d3260ea34764b8415d90
SamJones1995 <88803371+SamJones1995@users.noreply.github.com>
2021-11-27 10:58:34 -0500

    Added directors array, directors function, new user function, and update username function

--- index.js
+++ index.js
@@ -28,6 +28,15 @@ let subgenres = [
   }
 ]
 
+let directors = [
+  {
+    name: 'David Bruckner',
+    bio: 'David Bruckner (born c. 1977) is an American film director. With Jacob Gentry and Dan Bush, he co-wrote and co-directed The Signal (2007).',
+    birthyear: '1977',
+    deathyear: 'N/A'
+  }
+]
+
 app.get('/',(req,res) => {
   res.send('Welcome to myHorror!');
 });
@@ -47,6 +56,38 @@ app.get('/horrorMovies/:title', (req,res) => {
 app.get('/subgenres/:name', (req,res) => {
   res.json(subgenres.find( (subgenre) =>
     {return subgenre.name === req.params.name}));
+});
+
+//show a director's information by name
+app.get('/directors/:name', (req,res) => {
+  res.json(directors.find( (director) =>
+    {return director.name === req.params.name}));
+});
+
+//Allow new user to register
+app.post('/users', (req,res) => {
+  let newUser = req.body;
+
+  if(!newUser.name) {
+    const message = 'Missing username';
+    res.status(400).send(message);
+  } else {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).send(newUser);
+  }
+});
+
+//Allow user to update username
+app.put('/users/:username', (req,res) => {
+  let user = users.find((user) => { return user.name === req.params.name});
+
+  if (user) {
+    user.username[req.params.username] = parseInt(req.params.grade);
+    res.status(201).send('User ' + req.params.username + ' was updated. ');
+  } else {
+    res.status(404).send('User ' + req.params.name + ' was not found.');
+  }
 });
 
 //middleware
