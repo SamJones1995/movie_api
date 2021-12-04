24121f78265ad81abf37fdd4ee56b9cf2a6a387b
SamJones1995 <88803371+SamJones1995@users.noreply.github.com>
2021-12-04 11:26:28 -0500

    Updated model to lowercase 'e' email and 'Birthdate' user fields to match database

--- models.js
+++ models.js
@@ -18,8 +18,8 @@ let movieSchema = mongoose.Schema({
 let userSchema = mongoose.Schema({
   Username: {type: String, required: true},
   Password: {type: String, required: true},
-  Email: {type: String, required: true},
-  Birthday: Date,
+  email: {type: String, required: true},
+  Birthdate: Date,
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
 
