868c3c4d6d890999bf48bd945a42d8dc774ac3df
SamJones1995 <88803371+SamJones1995@users.noreply.github.com>
2022-03-01 21:45:22 -0500

    Removed CORS for testing

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const uuid = ('uuid');
 const app = express();
 const { check, validationResult } = require('express-validator');
 const mongoose = require('mongoose');
-const cors = require('cors');
+//const cors = require('cors');
 
 
 const Models = require('./models.js');
@@ -13,7 +13,7 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 
-app.use(cors());
+//app.use(cors());
 
 //mongoose.connect('mongodb://localhost:27017/myHorrorDB', {useNewUrlParser: true, useUnifiedTopology: true }); // local environment
 mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true });
